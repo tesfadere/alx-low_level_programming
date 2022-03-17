a972e180783912a488b76a878342047fd9b327df
tesfadere <tesfadere1@gmail.com>
2022-03-17 20:02:35 +0000

    standard error output

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+return (1);
+}
